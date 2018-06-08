3773e649399cb11b995c4eae020f24f6d790a559
poom <phurichp@ptis.ac.th>
2018-06-08 14:33:01 +1000

    Initial release test - change front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Maeploy</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,8 +79,10 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    BoatKuayLek
                 </div>
+
+                <p style="text-align: center; padding-bottom: 20px"> จ่ายกู <b>20</b> ดอลล่าห์ด้วยนะควย</p>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
